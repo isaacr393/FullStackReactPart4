68814c902fa375eab150c2fc02a2e0747c4384c8
Isaac <isaacrivas140@gmail.com>
2021-11-20 18:54:02 -0400

    4.6

--- Utils/list_helper.js
+++ Utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => blogs.reduce((prev, blog) => {
@@ -8,8 +10,19 @@ const favorite = (blogs) => blogs.reduce((prev, blog) => {
     return blog.likes > prev.likes? blog:prev
 })
 
+const mostBlogs = ( blogs ) => {
+    let grouped = _.groupBy(blogs, 'author');
+    return Object.keys(grouped).reduce( (prev, key) => {
+        if( prev.blogs < grouped[key].length )
+        return { author: key, blogs: grouped[key].length }
+        else
+        return prev
+    }, {author: 'None', blogs: 0 })
+}
+
 module.exports = {
     dummy,
     totalLikes,
     favorite,
+    mostBlogs
 }
